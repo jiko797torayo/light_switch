b65045ddc64472c6176224aa73ebe4ee7412d939
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-03 00:27:39 +0900

    Initial Commit

---
+++ Light-Switch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light-Switch
+//
+//  Created by 大石耕司 on 2019/06/03.
+//  Copyright © 2019 大石耕司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
