14c185b48aecc8707dfd403e0bcdfa0377863a70
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-08 13:33:04 +0900

    Light-Switchベース作成

--- Light-Switch/ViewController.swift
+++ Light-Switch/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isOn = true
+    @IBOutlet weak var lightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        if isOn == true {
+            view.backgroundColor = .white
+            lightLabel.text = "Light On"
+            lightLabel.textColor = .black
+            isOn = false
+        } else {
+            view.backgroundColor = .black
+            lightLabel.text = "Light Off"
+            lightLabel.textColor = .white
+            isOn = true
+        }
+    }
+    
 }
 
